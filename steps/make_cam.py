5a65051c0b82c38d8c6c8cef4e85ab6cb252c4d3
Cenk Bircanoglu <cenk.bircanoglu@adevinta.com>
2021-03-31 11:18:56 +0200

    update make_cam script to support unet/u2net and u2netp

--- steps/make_cam.py
+++ steps/make_cam.py
@@ -12,14 +12,35 @@ from tqdm import tqdm
 from voc12 import dataloader
 
 
-@hydra.main(config_path='../conf', config_name="classifier/make_cam")
+def extract_valid_cams(cams, size, label, cfg, idx, img_name):
+    idx = str(idx)
+    strided_cam = torch.sum(
+        torch.stack(
+            [F.interpolate(o, size, mode='bilinear', align_corners=False)[0] for o in cams]),
+        0)
+
+    valid_cat = torch.nonzero(label, as_tuple=False)[:, 0]
+
+    strided_cam = strided_cam[valid_cat]
+    strided_cam /= F.adaptive_max_pool2d(strided_cam, (1, 1)) + 1e-5
+    output_folder = os.path.join(cfg.output_dir, idx)
+    os.makedirs(output_folder, exist_ok=True)
+    np.save(os.path.join(output_folder, img_name + '.npy'),
+            {"keys": valid_cat, "high_res": strided_cam.cpu().numpy()})
+
+
+@hydra.main(config_path='../conf', config_name="tests/make_cam/unet")
 def run_app(cfg: DictConfig) -> None:
     os.makedirs(cfg.output_dir, exist_ok=True)
     device = torch.device("cpu" if not torch.cuda.is_available() else cfg.device)
 
     data_loader = data_loaders(cfg)
-    model = getattr(importlib.import_module(cfg.model), 'CAM')(in_ch=3, mid_ch=cfg.mid_ch, out_ch=1,
-                                                               share_classifier=cfg.share_classifier)
+    model = getattr(importlib.import_module(cfg.model), 'CAM')(
+        in_ch=3,
+        mid_ch=cfg.mid_ch,
+        out_ch=cfg.out_ch,
+        num_classes=cfg.num_classes,
+        share_classifier=cfg.share_classifier)
     model.load_state_dict(torch.load(cfg.weights, map_location='cpu'), strict=True)
     model.to(device)
     model.eval()
@@ -32,19 +53,13 @@ def run_app(cfg: DictConfig) -> None:
         label = label.to(device)
 
         with torch.set_grad_enabled(False):
-            # c0, c1, c2, c3, c4, c5, c6
-            cams = list(zip(*[model(img[0].to(device)) for img in imgs]))
-            selected_cam = cams[cfg.cam_number]
-            strided_cam = torch.sum(
-                torch.stack([F.interpolate(o, size, mode='bilinear', align_corners=False)[0] for o in selected_cam]), 0)
-
-            valid_cat = torch.nonzero(label)[:, 0]
-
-            strided_cam = strided_cam[valid_cat]
-            strided_cam /= F.adaptive_max_pool2d(strided_cam, (1, 1)) + 1e-5
-
-            np.save(os.path.join(cfg.output_dir, img_name + '.npy'),
-                    {"keys": valid_cat, "high_res": strided_cam.cpu().numpy()})
+            cams = [model(img[0].to(device)) for img in imgs]
+            if type(cams[0]) == tuple:
+                cams = list(zip(*cams))
+                for idx, selected_cams in enumerate(cams):
+                    extract_valid_cams(selected_cams, size, label, cfg, idx, img_name)
+            else:
+                extract_valid_cams(cams, size, label, cfg, 0, img_name)
 
 
 def data_loaders(cfg):
